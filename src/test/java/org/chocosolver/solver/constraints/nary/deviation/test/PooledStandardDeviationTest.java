e0edd379daf18b38e5e66c2e45c576ffe4064a28
gwr3n <robros@gmail.com>
2017-12-17 21:43:11 +0000

    Added PooledStandardDeviationTest

---
+++ src/test/java/org/chocosolver/solver/constraints/nary/deviation/test/PooledStandardDeviationTest.java
@@ -0,0 +1,206 @@
+package org.chocosolver.solver.constraints.nary.deviation.test;
+
+import static org.junit.Assert.*;
+
+import org.chocosolver.samples.AbstractProblem;
+import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.constraints.nary.deviation.PooledStandardDeviation;
+import org.chocosolver.solver.search.strategy.IntStrategyFactory;
+import org.chocosolver.solver.search.strategy.selectors.values.RealDomainMiddle;
+import org.chocosolver.solver.search.strategy.selectors.variables.Cyclic;
+import org.chocosolver.solver.search.strategy.strategy.AbstractStrategy;
+import org.chocosolver.solver.search.strategy.strategy.RealStrategy;
+import org.chocosolver.solver.variables.IntVar;
+import org.chocosolver.solver.variables.RealVar;
+import org.chocosolver.solver.variables.VariableFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PooledStandardDeviationTest {
+
+   @Before
+   public void setUp() throws Exception {
+   }
+
+   @After
+   public void tearDown() throws Exception {
+   }
+
+   @Test
+   public void testInteger() {
+      String[] str={"-log","SOLUTION"};
+      
+      int[][] valuesA = {{1},{2},{3},{4},{5},{6},{7},{8},{9}};
+      int[][] valuesB = {{5},{4},{6},{5},{4},{6},{5},{4},{6}};
+      
+      IntegerPooledStandardDeviation standardDeviation = new IntegerPooledStandardDeviation(valuesA, valuesB, new double[]{0,100});
+      standardDeviation.execute(str);
+   }
+   
+   @Test
+   public void testReal() {
+      String[] str={"-log","SOLUTION"};
+      
+      double[][] valuesA = {{1},{2},{3},{4},{5},{6},{7},{8},{9}};
+      double[][] valuesB = {{5},{4},{6},{5},{4},{6},{5},{4},{6}};
+      
+      RealPooledStandardDeviation standardDeviation = new RealPooledStandardDeviation(valuesA, valuesB, new double[]{0,100});
+      standardDeviation.execute(str);
+   }
+
+   class IntegerPooledStandardDeviation extends AbstractProblem {
+      public IntVar[] valueVariablesA;
+      public IntVar[] valueVariablesB;
+      public RealVar standardDeviationVariable;
+      
+      public int[][] valuesA;
+      public int[][] valuesB;
+      public double[] standardDeviation;
+      
+      double precision = 1.e-4;
+      
+      public IntegerPooledStandardDeviation(int[][] valuesA, int[][] valuesB, double[] standardDeviation){
+         this.valuesA = valuesA;
+         this.valuesB = valuesB;
+         this.standardDeviation = standardDeviation;
+      }
+      
+      @Override
+      public void createSolver() {
+         solver = new Solver("IntegerPooledStandardDeviation");
+      }
+      
+      @Override
+      public void buildModel() {
+         valueVariablesA = new IntVar[this.valuesA.length];
+         for(int i = 0; i < this.valuesA.length; i++)
+            valueVariablesA[i] = VariableFactory.enumerated("ValueA"+(i+1), valuesA[i], solver);
+         
+         valueVariablesB = new IntVar[this.valuesB.length];
+         for(int i = 0; i < this.valuesB.length; i++)
+            valueVariablesB[i] = VariableFactory.enumerated("ValueB"+(i+1), valuesB[i], solver);
+         
+         standardDeviationVariable = VariableFactory.real("PooledStandardDeviation", standardDeviation[0], standardDeviation[1], precision, solver);
+         
+         PooledStandardDeviation.decompose("PooledStandardDeviationConstraint", valueVariablesA, valueVariablesB, standardDeviationVariable, precision);
+      }
+      
+      @Override
+      public void configureSearch() {
+         AbstractStrategy<IntVar> stratA = IntStrategyFactory.activity(valueVariablesA,1234);
+         AbstractStrategy<IntVar> stratB = IntStrategyFactory.activity(valueVariablesB,1234);
+         solver.set(stratA, stratB);
+      }
+    
+      @Override
+      public void solve() {
+        StringBuilder st = new StringBuilder();
+        boolean solution = solver.findSolution();
+        //do{
+           st.append("---\n");
+           if(solution) {
+              for(int i = 0; i < valueVariablesA.length; i++){
+                 st.append(valueVariablesA[i].getValue()+", ");
+              }
+              st.append("\n");
+              for(int i = 0; i < valueVariablesB.length; i++){
+                 st.append(valueVariablesB[i].getValue()+", ");
+              }
+              st.append("\n");
+              st.append(standardDeviationVariable.getLB()+" "+standardDeviationVariable.getUB());
+              st.append("\n");
+              
+              assertTrue(standardDeviationVariable.getLB() <= Math.sqrt(4.125));
+              assertTrue(standardDeviationVariable.getUB() >= Math.sqrt(4.125));
+           }else{
+              st.append("No solution!");
+           }
+        //}while(solution = solver.nextSolution());
+        System.out.println(st.toString());
+      }
+      
+      @Override
+      public void prettyOut() {
+          
+      }
+   }
+   
+   class RealPooledStandardDeviation extends AbstractProblem {
+      public RealVar[] valueVariablesA;
+      public RealVar[] valueVariablesB;
+      public RealVar standardDeviationVariable;
+      
+      public double[][] valuesA;
+      public double[][] valuesB;
+      public double[] standardDeviation;
+      
+      double precision = 1.e-4;
+      
+      public RealPooledStandardDeviation(double[][] valuesA, double[][] valuesB, double[] standardDeviation){
+         this.valuesA = valuesA;
+         this.valuesB = valuesB;
+         this.standardDeviation = standardDeviation;
+      }
+      
+      @Override
+      public void createSolver() {
+         solver = new Solver("RealPooledStandardDeviation");
+      }
+      
+      @Override
+      public void buildModel() {
+         valueVariablesA = new RealVar[this.valuesA.length];
+         for(int i = 0; i < this.valuesA.length; i++)
+            valueVariablesA[i] = VariableFactory.real("ValueA"+(i+1), valuesA[i][0], valuesA[i][0], precision, solver);
+         
+         valueVariablesB = new RealVar[this.valuesB.length];
+         for(int i = 0; i < this.valuesB.length; i++)
+            valueVariablesB[i] = VariableFactory.real("ValueB"+(i+1), valuesB[i][0], valuesB[i][0], precision, solver);
+         
+         standardDeviationVariable = VariableFactory.real("PooledStandardDeviation", standardDeviation[0], standardDeviation[1], precision, solver);
+         
+         PooledStandardDeviation.decompose("PooledStandardDeviationConstraint", valueVariablesA, valueVariablesB, standardDeviationVariable, precision);
+      }
+      
+      @Override
+      public void configureSearch() {
+         solver.set(
+               new RealStrategy(valueVariablesA, new Cyclic(), new RealDomainMiddle()),
+               new RealStrategy(valueVariablesB, new Cyclic(), new RealDomainMiddle()));
+      }
+    
+      @Override
+      public void solve() {
+        StringBuilder st = new StringBuilder();
+        boolean solution = solver.findSolution();
+        //do{
+           st.append("---\n");
+           if(solution) {
+              for(int i = 0; i < valueVariablesA.length; i++){
+                 st.append("("+valueVariablesA[i].getLB()+", "+valueVariablesA[i].getUB() + ")\t");
+              }
+              st.append("\n");
+              for(int i = 0; i < valueVariablesB.length; i++){
+                 st.append("("+valueVariablesB[i].getLB()+", "+valueVariablesB[i].getUB() + ")\t");
+              }
+              st.append("\n");
+              st.append(standardDeviationVariable.getLB()+" "+standardDeviationVariable.getUB());
+              st.append("\n");
+              
+              assertTrue(standardDeviationVariable.getLB() <= Math.sqrt(4.125));
+              assertTrue(standardDeviationVariable.getUB() >= Math.sqrt(4.125));
+           }else{
+              st.append("No solution!");
+           }
+        //}while(solution = solver.nextSolution());
+        System.out.println(st.toString());
+      }
+      
+      @Override
+      public void prettyOut() {
+          
+      }
+   }
+}
+
