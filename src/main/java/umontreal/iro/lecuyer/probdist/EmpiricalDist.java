87c13e04df1b9300c34c23e63601b4ae59c6ca19
gwr3n <robros@gmail.com>
2017-12-28 19:46:41 +0100

    Added MIT license to all files

--- src/main/java/umontreal/iro/lecuyer/probdist/EmpiricalDist.java
+++ src/main/java/umontreal/iro/lecuyer/probdist/EmpiricalDist.java
@@ -1,27 +1,27 @@
-
-
 /*
- * Class:        EmpiricalDist
- * Description:  empirical discrete distribution function
- * Environment:  Java
- * Software:     SSJ
- * Copyright (C) 2001  Pierre L'Ecuyer and Universit�� de Montr��al
- * Organization: DIRO, Universit�� de Montr��al
- * @author
- * @since
-
- * SSJ is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License (GPL) as published by the
- * Free Software Foundation, either version 3 of the License, or
- * any later version.
-
- * SSJ is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * A copy of the GNU General Public License is available at
-   <a href="http://www.gnu.org/licenses">GPL licence site</a>.
+ * syat-choco: a Choco extension for Declarative Statistics.
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2016 Roberto Rossi
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package umontreal.iro.lecuyer.probdist;
